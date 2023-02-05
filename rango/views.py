30a04c1002ecd67a32aa17469fc18505c023072c
“David <2604833k@student.gla.ac.uk>
2023-02-05 22:46:58 +0000

    Felt like a wee commit cant lie x

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='rango/about'> Link</a>")
+
+    context_dict ={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context = context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <a href='/'> Link </a>")
