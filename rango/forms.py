989e346d8b332d47f33ee109dd436525bb881ea7
“David <2604833k@student.gla.ac.uk>
2023-02-09 04:07:53 +0000

    must sleep. 9 is long and annoying

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,7 @@
 
 from django import forms
-from rango.models import Page, Category
+from rango.models import *
+from django.contrib.auth.models import User
 char_field_max_length = 128
 url_field_max_length = 200
 class CategoryForm(forms.ModelForm):
@@ -31,3 +32,16 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website','picture')
