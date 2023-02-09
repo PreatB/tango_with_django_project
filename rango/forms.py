013bc978fbb7e90740f6a6428e8bf3ee8ab9c460
“David <2604833k@student.gla.ac.uk>
2023-02-09 00:57:37 +0000

    End of chapter 7. My life is becoming cloudy. Got called into work when they said i could have a day off. i spend 50 minutes in rush hour traffic and when i get there, there is no work. literally went home in 3 hours when it is normally an 8 hour shift. waste of everyones time honestly. 5 red bull (sugar free) deep right now and i want this done. heart failure before resitting the year

---
+++ rango/forms.py
@@ -0,0 +1,33 @@
+
+from django import forms
+from rango.models import Page, Category
+char_field_max_length = 128
+url_field_max_length = 200
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=char_field_max_length, help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required=False)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required= False)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+
+        model = Category
+        fields = ('name',)
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=char_field_max_length, help_text = "Please enter the title of the page.")
+    url = forms.URLField(max_length=url_field_max_length, help_text="Please enter the URL of the page.")
+    views  = forms.IntegerField(widget=forms.HiddenInput(),initial=0, required=False)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
